96a1eea2b7fad908c1536f235a4a2533aa0a55c5
IhorVeIychko <igorvelicko148@gmail.com>
2025-10-15 15:38:40 +0200

    HelloVistula

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public static void main(String[] args) {
+    //variables
+    int a = 5;
+    double b = 5.0;
+    String c = "Hello World!";
+    boolean isActive = true;
+
+    //print methods
+    System.out.println("Hello Vistula!");
+    System.out.print("Hello world"); //print without \n
+
+    //conditions
+    if (a >= 5){
+        System.out.println("Hello World!");
+    } else{
+        System.out.println("Hello World!");
+    }
+
+    //for loop
+    for (int i = 0; i < 5; i++) {
+        System.out.println("Count: " + i);
+    }
+
+}
